13b0889c3faf3b8ca48a0984ac9e017a8913a983
PriyadharshniA <49320034+PriyadharshniA@users.noreply.github.com>
2020-02-27 11:55:19 -0800

    updated comments
    added wait time in the test steps and added comments

--- WebAutomation_Project/Pages/loginPage.py
+++ WebAutomation_Project/Pages/loginPage.py
@@ -16,9 +16,15 @@ class LoginPage():
 
     # Create class methods for each test steps
     def setSignIn(self):
+
+        # application is very slow most of the time, hence adding sleep time almost after every action!
+
+        time.sleep(6)
         self.driver.find_element_by_xpath(self.email_textbox).send_keys("qabootcamp20@gmail.com")
+        time.sleep(2)
         self.driver.find_element_by_xpath(self.password_textbox).send_keys("QAbootcampPWD1")
         # wait = WebDriverWait(self.driver, 5)
         # wait.until(EC.element_to_be_clickable(By.ID, self.login_button)).click()
-        self.driver.implicitly_wait(10)
+        time.sleep(3)
         self.driver.find_element_by_id(self.login_button).click()
+        time.sleep(3)
