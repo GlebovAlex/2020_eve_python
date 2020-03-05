a4044d42e5075dc177e7194278afe9bdfaa0834b
PriyadharshniA <49320034+PriyadharshniA@users.noreply.github.com>
2020-03-05 15:28:17 -0800

    Added Test Suite
    Added test cases to the test suite and to the test runner.

--- WebAutomation_Project/main.py
+++ WebAutomation_Project/main.py
@@ -1,3 +1,22 @@
-import subprocess
+import unittest
+import HtmlTestRunner
+from TestCases.TC_addNewTalent import TestPromytheus_AddNewTalent
+from TestCases.TC_Login import TestPromytheus_Login
+from TestCases.TC_InvalidSignIn import Test_InvalidSignIn
 
-subprocess.call("C:\Users\priya\Promytheus\TestCases\TC_addNewTalent.py")
+# initialize test suite, test loader and test runner
+suite = unittest.TestSuite()
+loader = unittest.TestLoader()
+runner = HtmlTestRunner.HTMLTestRunner(output="C:/Users/priya/WebAutomation_Project/Reports")
+
+# get all tests
+addNewTalent = loader.loadTestsFromTestCase(TestPromytheus_AddNewTalent)
+loginSignIn = loader.loadTestsFromTestCase(TestPromytheus_Login)
+invalidSignIn = loader.loadTestsFromTestCase(Test_InvalidSignIn)
+
+# creating Test Suites
+smokeTestSuite = unittest.TestSuite([addNewTalent])
+functionalTestSuite = unittest.TestSuite([loginSignIn, addNewTalent, invalidSignIn])
+
+# running the test suite
+runner.run(smokeTestSuite)
