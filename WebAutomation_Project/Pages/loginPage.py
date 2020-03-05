f4f85d9fd006ef01d65a3b10a6b2ec76d2c15489
PriyadharshniA <49320034+PriyadharshniA@users.noreply.github.com>
2020-03-05 15:32:19 -0800

    added invalid SignIn tests
    added negative test cases that checks invalid Sign In scenarios

--- WebAutomation_Project/Pages/loginPage.py
+++ WebAutomation_Project/Pages/loginPage.py
@@ -28,3 +28,35 @@ class LoginPage():
         time.sleep(3)
         self.driver.find_element_by_id(self.login_button).click()
         time.sleep(3)
+
+    def invalidSignIn(self):
+
+        # performs invalid sign in and checks if a warning message is displayed to the user
+
+        time.sleep(6)
+        self.driver.find_element_by_xpath(self.email_textbox).send_keys("qabootcamp20@gmail.com")
+        time.sleep(3)
+        self.driver.find_element_by_id(self.login_button).click()
+        time.sleep(3)
+
+    def invalidEmail(self):
+
+        # performs invalid sign in with a invalid email id value
+        time.sleep(6)
+        self.driver.find_element_by_xpath(self.email_textbox).send_keys("qabootcamp20")
+        time.sleep(2)
+        self.driver.find_element_by_xpath(self.password_textbox).send_keys("QAbootcampPWD1")
+        time.sleep(3)
+        self.driver.find_element_by_id(self.login_button).click()
+        time.sleep(3)
+
+    def unregisteredEmail(self):
+
+        # performs sign in using a valid email-id but not registered with Promytheus
+        time.sleep(6)
+        self.driver.find_element_by_xpath(self.email_textbox).send_keys("priya3010@gmail.com")
+        time.sleep(2)
+        self.driver.find_element_by_xpath(self.password_textbox).send_keys("Qwerty@1230")
+        time.sleep(3)
+        self.driver.find_element_by_id(self.login_button).click()
+        time.sleep(3)
